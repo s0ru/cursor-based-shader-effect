29873e47946537ad7a76e60165519c6f44d29e4c
s0ru <apazgarcia23@outlook.es>
2024-05-23 19:47:52 +0200

    keep track of mouse position over picture and draw texture on canvas

--- src/script.js
+++ src/script.js
@@ -7,6 +7,40 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
+const displacement = {}
+// HTML SIDE
+displacement.canvas = document.createElement('canvas')
+displacement.canvas.width = 128
+displacement.canvas.height = 128
+displacement.context = displacement.canvas.getContext('2d')
+displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+displacement.glowImage = new Image()
+displacement.glowImage.src = './glow.png'
+displacement.canvas.style.position = 'fixed'
+displacement.canvas.style.width = '256px'
+displacement.canvas.style.height = '256px'
+displacement.canvas.style.top = 0
+displacement.canvas.style.left = 0
+displacement.canvas.style.zIndex = 10
+document.body.append(displacement.canvas)
+// THREE SIDE
+displacement.interactivePlane = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+displacement.interactivePlane.visible = false;
+scene.add(displacement.interactivePlane)
+displacement.raycaster = new THREE.Raycaster()
+displacement.screenCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+// LISTENERS
+window.addEventListener('pointermove', (event) =>
+{
+    displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
+    displacement.screenCursor.y = - (event.clientY / sizes.height) * 2 + 1
+})
+
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
@@ -60,6 +94,32 @@ const tick = () =>
 {
     controls.update()
     renderer.render(scene, camera)
+
+    displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
+
+    if(intersections.length)
+    {
+        const uv = intersections[0].uv
+        displacement.canvasCursor.x = uv.x * displacement.canvas.width
+        displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height
+    }
+
+    displacement.context.globalCompositeOperation = 'source-over'
+    displacement.context.globalAlpha = 0.1
+    displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+
+    const glowSize = displacement.canvas.width * 0.25
+    displacement.context.globalCompositeOperation = 'lighten'
+    displacement.context.globalAlpha = 1
+    displacement.context.drawImage(
+        displacement.glowImage,
+        displacement.canvasCursor.x - glowSize * 0.5,
+        displacement.canvasCursor.y - glowSize * 0.5,
+        glowSize,
+        glowSize
+    )
+
     window.requestAnimationFrame(tick)
 }
 
